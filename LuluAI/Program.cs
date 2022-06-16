d01712f67804f8f0d43eb06ad710d710d3c7b330
Long Kim Hoang <longkimhoang@outlook.com>
2022-06-16 23:47:31 +0700

    Clean up code

--- LuluAI/Program.cs
+++ LuluAI/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Reflection;
 
 using IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, configuration) =>
