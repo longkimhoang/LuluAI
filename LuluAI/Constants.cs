88de2218dac4035336bdcac39c4a491a287e02f3
Long Kim Hoang <longkimhoang@outlook.com>
2022-06-18 22:35:43 +0700

    Add lulued command

---
+++ LuluAI/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuluAI;
+
+public class Constants
+{
+    public class S3
+    {
+        public static readonly string BucketName = "lulu-ai";
+    }
+}
