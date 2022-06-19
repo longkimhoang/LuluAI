65b958523d1d7b49cfa3296c8b79b1325189cf2a
Long Kim Hoang <longkimhoang@outlook.com>
2022-06-19 22:39:05 +0700

    Update lulued command to use s3 public url instead

---
+++ LuluAI/Extensions/S3ObjectExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Amazon.S3.Model;
+using LuluAI.Options;
+
+namespace LuluAI.Extensions;
+
+public static class S3ObjectExtensions
+{
+    public static string GetPublicUrl(this S3Object s3Object, AmazonS3Options options)
+    {
+        return $"https://{options.BucketName}.s3.amazonaws.com/{s3Object.Key}";
+    }
+}
